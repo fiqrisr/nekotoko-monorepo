49762103bafbe5a70de65f96b8717391127db79a
Fiqri Syah Redha <wibulo27@gmail.com>
2023-10-31 23:49:52 +0700

    chore: fix rootDir errors

---
+++ apps/svc-auth/.eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  ignorePatterns: [".eslintrc.js"]
+};
--- apps/svc-auth/src/main.ts
+++ apps/svc-auth/src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from "@nestjs/core";
+import { NestFactory, HttpAdapterHost } from "@nestjs/core";
 import { Logger } from "nestjs-pino";
+import { AllExceptionFilter } from "@nekotoko/shared-nest-utils";
+
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
   app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
